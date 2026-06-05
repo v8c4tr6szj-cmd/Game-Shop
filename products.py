539fc676c802719e88899e2783071295efc25b6f
v8c4tr6szj-cmd <v8c4tr6szj@privaterelay.appleid.com>
2026-06-05 12:11:09 +0500

    Create products.py
    PRODUCTS = {
        "pubg": {
            "name": "🎮 PUBG UC",
            "items": [
                {"id": "pubg_60", "name": "60 UC", "price": 1.0},
                {"id": "pubg_300", "name": "300 UC", "price": 4.5},
                {"id": "pubg_600", "name": "600 UC", "price": 9.0},
                {"id": "pubg_1800", "name": "1800 UC", "price": 25.0},
            ]
        },
        "freefire": {
            "name": "💎 Free Fire Diamond",
            "items": [
                {"id": "ff_100", "name": "100 Diamond", "price": 1.0},
                {"id": "ff_310", "name": "310 Diamond", "price": 2.4},
                {"id": "ff_520", "name": "520 Diamond", "price": 4.0},
                {"id": "ff_1060", "name": "1060 Diamond", "price": 8.0},
            ]
        },
        "roblox": {
            "name": "🟡 Roblox Robux",
            "items": [
                {"id": "roblox_400", "name": "400 Robux", "price": 5.0},
                {"id": "roblox_800", "name": "800 Robux", "price": 10.0},
                {"id": "roblox_1700", "name": "1700 Robux", "price": 20.0},
            ]
        },
        "steam": {
            "name": "🎁 Steam Gift Card",
            "items": [
                {"id": "steam_5", "name": "$5 Steam", "price": 6.0},
                {"id": "steam_10", "name": "$10 Steam", "price": 11.0},
                {"id": "steam_20", "name": "$20 Steam", "price": 22.0},
            ]
        },
        "mlbb": {
            "name": "💠 Mobile Legends",
            "items": [
                {"id": "mlbb_86", "name": "86 Diamond", "price": 1.5},
                {"id": "mlbb_172", "name": "172 Diamond", "price": 3.0},
                {"id": "mlbb_429", "name": "429 Diamond", "price": 7.0},
            ]
        },
        "brawl": {
            "name": "💎 Brawl Stars",
            "items": [
                {"id": "brawl_30", "name": "30 Gems", "price": 2.0},
                {"id": "brawl_80", "name": "80 Gems", "price": 5.0},
                {"id": "brawl_170", "name": "170 Gems", "price": 10.0},
            ]
        },
        "clash": {
            "name": "⚔️ Clash of Clans",
            "items": [
                {"id": "coc_80", "name": "80 Gems", "price": 1.0},
                {"id": "coc_500", "name": "500 Gems", "price": 5.0},
                {"id": "coc_1200", "name": "1200 Gems", "price": 10.0},
            ]
        },
        "valorant": {
            "name": "🔫 Valorant VP",
            "items": [
                {"id": "val_475", "name": "475 VP", "price": 5.0},
                {"id": "val_1000", "name": "1000 VP", "price": 10.0},
                {"id": "val_2050", "name": "2050 VP", "price": 20.0},
            ]
        },
        "fortnite": {
            "name": "🏆 Fortnite V-Bucks",
            "items": [
                {"id": "fn_1000", "name": "1000 V-Bucks", "price": 8.0},
                {"id": "fn_2800", "name": "2800 V-Bucks", "price": 20.0},
            ]
        },
        "genshin": {
            "name": "✨ Genshin Impact",
            "items": [
                {"id": "gs_60", "name": "60 Crystals", "price": 1.0},
                {"id": "gs_300", "name": "300 Crystals", "price": 5.0},
                {"id": "gs_980", "name": "980 Crystals", "price": 15.0},
            ]
        },
    }

---
+++ products.py
@@ -0,0 +1,83 @@
+PRODUCTS = {
+    "pubg": {
+        "name": "🎮 PUBG UC",
+        "items": [
+            {"id": "pubg_60", "name": "60 UC", "price": 1.0},
+            {"id": "pubg_300", "name": "300 UC", "price": 4.5},
+            {"id": "pubg_600", "name": "600 UC", "price": 9.0},
+            {"id": "pubg_1800", "name": "1800 UC", "price": 25.0},
+        ]
+    },
+    "freefire": {
+        "name": "💎 Free Fire Diamond",
+        "items": [
+            {"id": "ff_100", "name": "100 Diamond", "price": 1.0},
+            {"id": "ff_310", "name": "310 Diamond", "price": 2.4},
+            {"id": "ff_520", "name": "520 Diamond", "price": 4.0},
+            {"id": "ff_1060", "name": "1060 Diamond", "price": 8.0},
+        ]
+    },
+    "roblox": {
+        "name": "🟡 Roblox Robux",
+        "items": [
+            {"id": "roblox_400", "name": "400 Robux", "price": 5.0},
+            {"id": "roblox_800", "name": "800 Robux", "price": 10.0},
+            {"id": "roblox_1700", "name": "1700 Robux", "price": 20.0},
+        ]
+    },
+    "steam": {
+        "name": "🎁 Steam Gift Card",
+        "items": [
+            {"id": "steam_5", "name": "$5 Steam", "price": 6.0},
+            {"id": "steam_10", "name": "$10 Steam", "price": 11.0},
+            {"id": "steam_20", "name": "$20 Steam", "price": 22.0},
+        ]
+    },
+    "mlbb": {
+        "name": "💠 Mobile Legends",
+        "items": [
+            {"id": "mlbb_86", "name": "86 Diamond", "price": 1.5},
+            {"id": "mlbb_172", "name": "172 Diamond", "price": 3.0},
+            {"id": "mlbb_429", "name": "429 Diamond", "price": 7.0},
+        ]
+    },
+    "brawl": {
+        "name": "💎 Brawl Stars",
+        "items": [
+            {"id": "brawl_30", "name": "30 Gems", "price": 2.0},
+            {"id": "brawl_80", "name": "80 Gems", "price": 5.0},
+            {"id": "brawl_170", "name": "170 Gems", "price": 10.0},
+        ]
+    },
+    "clash": {
+        "name": "⚔️ Clash of Clans",
+        "items": [
+            {"id": "coc_80", "name": "80 Gems", "price": 1.0},
+            {"id": "coc_500", "name": "500 Gems", "price": 5.0},
+            {"id": "coc_1200", "name": "1200 Gems", "price": 10.0},
+        ]
+    },
+    "valorant": {
+        "name": "🔫 Valorant VP",
+        "items": [
+            {"id": "val_475", "name": "475 VP", "price": 5.0},
+            {"id": "val_1000", "name": "1000 VP", "price": 10.0},
+            {"id": "val_2050", "name": "2050 VP", "price": 20.0},
+        ]
+    },
+    "fortnite": {
+        "name": "🏆 Fortnite V-Bucks",
+        "items": [
+            {"id": "fn_1000", "name": "1000 V-Bucks", "price": 8.0},
+            {"id": "fn_2800", "name": "2800 V-Bucks", "price": 20.0},
+        ]
+    },
+    "genshin": {
+        "name": "✨ Genshin Impact",
+        "items": [
+            {"id": "gs_60", "name": "60 Crystals", "price": 1.0},
+            {"id": "gs_300", "name": "300 Crystals", "price": 5.0},
+            {"id": "gs_980", "name": "980 Crystals", "price": 15.0},
+        ]
+    },
+}
